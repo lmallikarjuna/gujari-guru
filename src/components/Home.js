7fd10b800538eac741d01b0e6555f8745ed4ab12
unknown <l.mallikarjunake@gmail.com>
2021-01-31 08:12:54 +0530

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,12 @@ export default function Home() {
   return (
     <div className="text-text">
       <div className="header-container">
-        <h1>
-          GUJARIGURU
-        </h1>
-        <p className='tag-line'>
-          One line here describing the work. One line here describing the work.
+        <h1>GUJARIGURU</h1>
+        <p className="tag-line">
+          Helping society to recycle more waste by enabling the recycling
+          sector.
         </p>
       </div>
-      </div>
+    </div>
   );
 }
